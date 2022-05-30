8e5256281f25b35d2cf1c4789234b14b23dd79ff
TRMgit <83818448+TRMgit@users.noreply.github.com>
2022-05-30 10:46:02 +0200

    Add files via upload

---
+++ YoutubeDownldSite/download.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+
+const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA',
+  // Optional arguments passed to youtube-dl.
+  ['--format=18'],
+)
+
+// Will be called when the download starts.
+video.on('info', function(info) {
+  console.log('Download started')
+  console.log('filename: ' + info._filename)
+  console.log('size: ' + info.size)
+})
+
+video.pipe(fs.createWriteStream('downloaded_vid.mp4'))
